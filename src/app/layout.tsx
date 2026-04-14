69353050f54c96b55dd932b5e025f3ea83edaf85
reactcodin <narmathi93@gmail.com>
2026-04-14 16:20:29 +0530

    Feat: Config updated successfully

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Portfolio | Narmathi GP",
   description:
-    "Portfolio of Narmathi GP, a Full Stack Developer skilled in React, Next.js, Node.js, TypeScript,Codeigniter and more. Explore projects, experience, and get in touch.",
+    "Portfolio of Narmathi GP, a Backend-focused Full Stack Developer skilled in Node.js, Next.js, Prisma, and PostgreSQL.",
   keywords: [
+    "Backend Developer",
     "Full Stack Developer",
-    "React",
-    "Next.js",
     "Node.js",
-    "TypeScript",
+    "Next.js",
+    "Prisma",
+    "PostgreSQL",
     "Portfolio",
   ],
   authors: [{ name: "Narmathi GP" }],
@@ -29,22 +19,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" className="dark">
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.16.0/devicon.min.css"
-        />
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
