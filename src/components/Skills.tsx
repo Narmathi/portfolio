b0843a40d392293f4e5973c2e1f9bd31e2d1259f
reactcodin <narmathi93@gmail.com>
2026-08-13 20:52:34 +0530

    Feat : Skill

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -13,15 +13,15 @@ const frontendSkills = [
   { name: "Next.js", icon: "devicon-nextjs-plain" },
   {
     name: "REST API Integration",
-    icon: <Plug />,
+    icon: "devicon-fastapi-plain",
   },
   {
     name: "Performance Optimization",
-    icon: <Gauge size={24} />,
+    icon: "devicon-webpack-plain",
   },
   {
     name: "Responsive Design",
-    icon: <MonitorSmartphone size={24} />,
+    icon: "devicon-css3-plain",
   },
 ];
 
@@ -43,6 +43,12 @@ const backendSkills = [
     icon: null,
     svgPath:
       "M21.807 18.285L13.553.756a1.324 1.324 0 00-1.129-.754 1.31 1.31 0 00-1.206.626l-8.952 14.5a1.356 1.356 0 00.016 1.455l4.376 6.778a1.408 1.408 0 001.58.581l12.703-3.757c.389-.115.707-.39.873-.755s.164-.783-.007-1.145zm-1.848.752L9.18 22.224a.452.452 0 01-.575-.52l3.85-18.438c.072-.345.549-.4.696-.08l7.992 17.4a.456.456 0 01-.384.65z",
+  },
+  {
+    name: "CodeIgniter 4",
+    icon: "devicon-codeigniter-plain",
+    svgPath:
+      "M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z",
   },
 ];
 
